a2a06a435f8280c52efd71f6f93a9cd6aef90ea0
RackiLovro <lovro.racki@fer.hr>
2021-01-13 20:01:40 +0100

    ...Scroll through early versions of screens

--- recepTour/DesktopTour/View/Homepage.xaml.cs
+++ recepTour/DesktopTour/View/Homepage.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DesktopTour
+namespace DesktopTour.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Homepage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Homepage : Window
     {
-        public MainWindow()
+        public Homepage()
         {
             InitializeComponent();
         }
