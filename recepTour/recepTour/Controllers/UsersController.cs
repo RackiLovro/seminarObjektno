c0cc4d4f0a82128c7238988b5645288e7301b68b
Klara Banić <klara.banic@fer.hr>
2021-01-15 18:58:44 +0100

    Implement registration

--- recepTour/recepTour/Controllers/UsersController.cs
+++ recepTour/recepTour/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -42,6 +44,42 @@ namespace recepTour.Controllers
             }
 
             return View(user);
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registration(UserRegistration uc)
+        {
+            User user = new User();
+            user.Nickname = uc.Nickname;
+            user.Email = uc.Email;
+            user.Status = "Just registered";
+            user.EmailConfirmed = false;
+            user.UserTypeId = _context.UserTypes.Where(d => d.UserType1 == "user").Select(d => d.Id).FirstOrDefault();
+            user.PasswordHash = EncodePasswordMd5(uc.Password);
+            _context.Add(user);
+            _context.SaveChanges();
+            ViewBag.message = "The user " + user.Nickname + " is saved successfully";
+            return View();
+        }
+
+
+        public static string EncodePasswordMd5(string pass) //Encrypt using MD5    
+        {
+            Byte[] originalBytes;
+            Byte[] encodedBytes;
+            MD5 md5;
+            //Instantiate MD5CryptoServiceProvider, get bytes for original password and compute hash (encoded password)    
+            md5 = new MD5CryptoServiceProvider();
+            originalBytes = ASCIIEncoding.Default.GetBytes(pass);
+            encodedBytes = md5.ComputeHash(originalBytes);
+            //Convert encoded bytes back to a 'readable' string    
+            return BitConverter.ToString(encodedBytes);
         }
 
         // GET: Users/Create
---
+++ recepTour/recepTour/Models/UserRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace recepTour.Models
+{
+    public partial class UserRegistration
+    {
+        public int Id { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string Nickname { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
