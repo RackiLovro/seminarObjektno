6265302c01bcb05eb2b40b67cd214d51d9dcd857
leon.suljic <leon.suljic@fer.hr>
2021-01-19 21:09:07 +0100

    Merge branch 'develop' of https://github.com/RackiLovro/seminarObjektno into develop

--- recepTour/DesktopTour/ViewModel/UserSearchViewModel.cs
+++ recepTour/DesktopTour/ViewModel/UserSearchViewModel.cs
@@ -1,12 +1,83 @@
-﻿using System;
+﻿using DesktopTour.Commands;
+using DesktopTour.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DesktopTour.ViewModel
 {
-    class UserSearchViewModel
+    class UserSearchViewModel : INotifyPropertyChanged
     {
+        private string _searchText;
+        private ObservableCollection<Recipe> _recipes;
+        private readonly DesktopTourContext _context;
+        public UserSearchViewModel()
+        {
+            _context = new DesktopTourContext();
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string info)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+
+        private ICommand _userSearch;
+        public ICommand UserSearch
+        {
+            get
+            {
+                if (_userSearch == null) _userSearch = new RelayCommand(o =>
+                {
+                    var username = o as string;
+                    var recipes = from r in _context.Recipes
+                                  join ur in _context.UserRecipes on r.Id equals ur.RecipeId
+                                  where ur.User.Nickname.Contains(username)
+                                  select new Recipe
+                                  {
+                                      Id = r.Id,
+                                      Title = r.Title,
+                                      DiffLevelId = r.DiffLevelId
+                                  };
+
+                    Recipes = new ObservableCollection<Recipe>(recipes.ToList());
+
+                },
+                    o => true
+                    );
+                return _userSearch;
+            }
+            set
+            {
+                _userSearch = value;
+            }
+        }
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged("SearchText");
+            }
+        }
+
+        public ObservableCollection<Recipe> Recipes
+        {
+            get { return _recipes; }
+            set
+            {
+                _recipes = value;
+                _recipes.ToList().ForEach(r => r.DiffLevel = _context.RecipeDifficulties.Where(rd => rd.DiffLevel == r.DiffLevelId).Single());
+                OnPropertyChanged("Recipes");
+            }
+        }
     }
 }
